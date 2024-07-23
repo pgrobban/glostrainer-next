7f42f16c0d7365124409e30f0d2e4989307148ec
Robert Sebescen <pgrobban@gmail.com>
2024-07-23 18:40:16 +0200

    added sorting to some table headers in TermList

--- src/components/TermList.tsx
+++ src/components/TermList.tsx
@@ -14,9 +14,12 @@ import {
   SxProps,
   Button,
 } from "@mui/material";
+import { visuallyHidden } from "@mui/utils";
 import AddIcon from "@mui/icons-material/Add";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
+import TableSortLabel from "@mui/material/TableSortLabel";
+import { useMemo, useState } from "react";
 
 interface Props {
   terms: Term[];
@@ -25,6 +28,8 @@ interface Props {
   onDeleteTerm: (term: Term) => void;
   sx?: SxProps;
 }
+
+type TermOrderable = "swedish" | "definition" | "type";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -46,6 +51,36 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
+const headCells = [
+  {
+    id: "swedish",
+    label: "Swedish",
+  },
+  {
+    id: "definition",
+    label: "Definition",
+  },
+  {
+    id: "type",
+    label: "Word class",
+  },
+];
+
+function descendingTermComparator(a: Term, b: Term, orderBy: TermOrderable) {
+  return b[orderBy].localeCompare(a[orderBy], "sv");
+}
+
+type Order = "asc" | "desc";
+
+function getTermComparator(
+  order: Order,
+  orderBy: TermOrderable
+): (a: Term, b: Term) => number {
+  return order === "desc"
+    ? (a, b) => descendingTermComparator(a, b, orderBy)
+    : (a, b) => -descendingTermComparator(a, b, orderBy);
+}
+
 const TermList: React.FC<Props> = ({
   sx,
   terms,
@@ -53,24 +88,54 @@ const TermList: React.FC<Props> = ({
   onEditTerm,
   onDeleteTerm,
 }) => {
+  const [orderBy, setOrderBy] = useState<TermOrderable | null>();
+  const [order, setOrder] = useState<"asc" | "desc">("asc");
+
+  const createSortHandler = (property: TermOrderable) => () => {
+    handleRequestSort(property);
+  };
+
+  const handleRequestSort = (property: TermOrderable) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
+
+  const sortedTerms = useMemo(
+    () =>
+      orderBy ? terms.slice().sort(getTermComparator(order, orderBy)) : terms,
+    [order, orderBy, terms]
+  );
+
   return (
     <TableContainer sx={sx} component={Paper}>
       <Table>
         <TableHead>
           <TableRow>
-            <StyledTableCell sx={{ fontWeight: 600 }}>Swedish</StyledTableCell>
-            <StyledTableCell sx={{ fontWeight: 600 }}>
-              Definition
-            </StyledTableCell>
-            <StyledTableCell sx={{ fontWeight: 600 }}>
-              Word class
-            </StyledTableCell>
+            {headCells.map((headCell) => (
+              <StyledTableCell key={headCell.id} sx={{ fontWeight: 600 }}>
+                <TableSortLabel
+                  active={orderBy === headCell.id}
+                  direction={orderBy === headCell.id ? order : "asc"}
+                  onClick={createSortHandler(headCell.id as TermOrderable)}
+                >
+                  {headCell.label}
+                  {orderBy === headCell.id ? (
+                    <Box component="span" sx={visuallyHidden}>
+                      {order === "desc"
+                        ? "sorted descending"
+                        : "sorted ascending"}
+                    </Box>
+                  ) : null}
+                </TableSortLabel>
+              </StyledTableCell>
+            ))}
             <StyledTableCell sx={{ fontWeight: 600 }}>Notes</StyledTableCell>
             <StyledTableCell width={100}>{/* actions */}</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {terms.map((term, index) => (
+          {sortedTerms.map((term, index) => (
             <StyledTableRow key={index}>
               <StyledTableCell>{term.swedish}</StyledTableCell>
               <StyledTableCell>{term.definition}</StyledTableCell>
